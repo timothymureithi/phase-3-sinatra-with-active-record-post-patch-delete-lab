433cdd8349ad6ec97921d8e2b85c7d2dfef9b73d
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-07 21:30:07 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/baked_goods/by_price' do
-    # see the BakedGood class for the  method definition of `.by_price`
     baked_goods = BakedGood.by_price
     baked_goods.to_json
   end
 
   get '/baked_goods/most_expensive' do
-    # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
@@ -31,7 +29,6 @@ class ApplicationController < Sinatra::Base
     )
     baked_good.to_json
   end
-
 
   patch '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
